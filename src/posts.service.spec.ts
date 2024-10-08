936cafc8b158d3480eaa6288090d95fa78a67f2c
smartis2816 <106492599+smartis2816@users.noreply.github.com>
2024-10-08 17:32:02 +0300

    Iteration 1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -21,12 +21,22 @@ describe('PostsService', () => {
 
     it('should return all posts if called without options', () => {
       // реализуйте тест-кейс
+      expect(postsService.findMany()).toEqual(posts);
     });
 
     it('should return correct posts for skip and limit options', () => {
       // реализуйте тест-кейс
+      const expectedPosts = postsService.findMany({skip: 1, limit: 2});
+      expect(expectedPosts).toEqual(posts.slice(1, 3));
     });
 
     // реализуйте недостающие тест-кейсы
+    it('should return correct posts for skip options', () => {
+      expect(postsService.findMany({skip: 1})).toEqual(posts.slice(1));
+    });
+
+    it('should return correct posts for limit options', () => {
+      expect(postsService.findMany({limit: 2})).toEqual(posts.slice(0, 2));
+    });
   });
 });
